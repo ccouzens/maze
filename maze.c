a6bd184fee2218db18faaebf2fefd43423ed6717
Chris Couzens <ccouzens@gmail.com>
2013-01-05 23:44:01 +0000

    Implement a nearly working maze generation algorithm.
    BUGS: It doesn't produce a valid maze.

--- maze.c
+++ maze.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <glib.h>
 
 typedef struct square {
 	char borderN;
@@ -62,30 +64,92 @@ void print_maze(maze *maze) {
 	printf("█ █\n");
 }
 
+void remove_maze_boundary(maze *m, int square_a, int square_b) {
+	if (square_a > square_b) {
+		int tmp = square_a;
+		square_a = square_b;
+		square_b = tmp;
+	}
+	if (square_b - square_a == 0) {
+		m->squares[square_b].borderE = 0;
+	} else {
+		m->squares[square_b].borderN = 0;
+	}
+}
+
+void create_passages(maze *m) {
+	// Set all borders to on
+	memset(m->squares, 1, m->width*m->height*sizeof(square));
+	GArray *explored = g_array_sized_new(FALSE, FALSE, sizeof(int), m->width*m->height);
+	int i =0;
+	g_array_append_val(explored, i);
+	enum square_status {EXPLORED=0, UNEXPLORED=1};
+	char square_statuses[m->width * m->height];
+	printf("sizeof(square_statuses) = %d\n", sizeof(square_statuses));
+	printf("UNEXPLORED= %d\n", UNEXPLORED);
+	printf("sizeof(square_status) = %zd\n", sizeof(enum square_status));
+	memset(square_statuses, (int)UNEXPLORED, sizeof(square_statuses));
+	square_statuses[0] = EXPLORED;
+	for(int i =0; i<21; i++) {
+		printf("square_statuses[%2i] = %i\n", i, square_statuses[i]);
+	}
+
+	while( explored->len ) {
+		int explored_index = g_random_int_range(0, explored->len);
+		int explored_cell_index = g_array_index(explored, int, explored_index);
+		int y = explored_cell_index/m->width;
+		int x = explored_cell_index - y*m->width;
+		printf("(x, y): (%2i, %2i)\n", x, y);
+		GArray *bordering_squares = g_array_sized_new(FALSE, FALSE, sizeof(int), 4);
+		int bordering_square;
+		if (y>0) {
+			bordering_square = explored_cell_index - m->width;
+			printf("bordering_square: %2i, square_statuses[%2i] = %i\n", bordering_square, bordering_square, square_statuses[bordering_square]);
+			if (square_statuses[bordering_square] == UNEXPLORED) {
+				g_array_append_val(bordering_squares, bordering_square);
+			}
+		}
+		if (x< m->width-1) {
+			bordering_square = explored_cell_index +1;
+			printf("bordering_square: %2i, square_statuses[%2i] = %i\n", bordering_square, bordering_square, square_statuses[bordering_square]);
+			if (square_statuses[bordering_square] == UNEXPLORED) {
+				g_array_append_val(bordering_squares, bordering_square);
+			}
+		}
+		if (y< m->height-1) {
+			bordering_square = explored_cell_index + m->width;
+			printf("bordering_square: %2i, square_statuses[%2i] = %i\n", bordering_square, bordering_square, square_statuses[bordering_square]);
+			if (square_statuses[bordering_square] == UNEXPLORED) {
+				g_array_append_val(bordering_squares, bordering_square);
+			}
+		}
+		if (x > 0) {
+			bordering_square = explored_cell_index -1;
+			printf("bordering_square: %2i, square_statuses[%2i] = %i\n", bordering_square, bordering_square, square_statuses[bordering_square]);
+			if (square_statuses[bordering_square] == UNEXPLORED) {
+				g_array_append_val(bordering_squares, bordering_square);
+			}
+		}
+		printf("bordering_squares->len: %i\n", bordering_squares->len);
+		if (bordering_squares->len==0) {
+			g_array_remove_index_fast(explored, explored_index);
+		} else {
+			bordering_square = g_array_index(
+				bordering_squares, int, g_random_int_range(0, bordering_squares->len)
+			);
+			remove_maze_boundary(m, bordering_square, explored_cell_index);
+			g_array_append_val(explored, bordering_square);
+			square_statuses[bordering_square] = EXPLORED;
+		}
+		g_array_free(bordering_squares, TRUE);
+	}
+
+	g_array_free(explored, TRUE);
+}
+
 int main(int argc, char **argv) {
-	maze *m = new_maze(3, 3);
-	
-	// Generate a simple maze
-	m->squares[0].borderN=0;
-	m->squares[0].borderE=1;
-	m->squares[1].borderN=1;
-	m->squares[1].borderE=0;
-	m->squares[2].borderN=1;
-	m->squares[2].borderE=0;
-
-	m->squares[3].borderN=1;
-	m->squares[3].borderE=1;
-	m->squares[4].borderN=0;
-	m->squares[4].borderE=0;
-	m->squares[5].borderN=1;
-	m->squares[5].borderE=0;
-
-	m->squares[6].borderN=0;
-	m->squares[6].borderE=1;
-	m->squares[7].borderN=1;
-	m->squares[7].borderE=0;
-	m->squares[8].borderN=1;
-	m->squares[8].borderE=0;
+	maze *m = new_maze(20, 7);
+	create_passages(m);
 	print_maze(m);
 	free_maze(m);
 	return 0;
